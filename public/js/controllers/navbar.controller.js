6628e9da5fe64f39eee1f41d74695ab1f9cb66cf
jahnava <jahnavalmt@gmail.com>
2017-04-17 09:48:25 -0400

    Modified home link

--- public/js/controllers/navbar.controller.js
+++ public/js/controllers/navbar.controller.js
@@ -7,4 +7,11 @@
   function NavbarController($scope, UserService){
     $scope.isLoggedIn = UserService.isLoggedIn;
   }
+
+  $scope.hcpurl = 'https://devlearnportal.herokuapp.com';
+
+$scope.linkModelFunc = function (url){
+  console.log('link model function');
+  $window.open(url);
+};
 }());
